83daf663cb8c5abdb6bbc806f09db58b9598076f
Sebastijan-Dominis <sebastijan.dominis99@gmail.com>
2026-01-04 20:02:16 +0100

    Trained and saved a repeated guest classification model, along with the feature importances, shap values, and the best f1 threshold.

--- models/classes_and_thresholds.py
+++ models/classes_and_thresholds.py
@@ -30,7 +30,23 @@ class FeatureEngineer(BaseEstimator, TransformerMixin):
         X['arrival_season'] = X['arrival_date_week_number'].apply(self.week_to_season)
         return X
     
+# Custom transformer for engineered features
+class FeatureEngineerRepeatedGuest(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+    
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        # total_stay
+        X['total_stay'] = X['stays_in_weekend_nights'] + X['stays_in_week_nights']
+        # adr_per_person
+        X['adr_per_person'] = X['adr'] / (X['adults'] + X['children'] + X['babies']).replace(0, 1)
+        return X
 
 # Define classification thresholds
 CANCELLATION_THRESHOLD = 0.4  # Threshold for classifying a booking as cancelled
 NO_SHOW_THRESHOLD = 0.6       # Threshold for classifying a booking as no-show
+REPEATED_GUEST_THRESHOLD = 0.76  # Threshold for classifying a booking as a repeated guest
